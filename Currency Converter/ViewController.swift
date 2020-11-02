8ca29626d754e59d56469bae1ece18d1331cb16e
kentonambara <kentonambara@gmail.com>
2020-11-02 16:18:38 -0500

    Initial commit

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Kento Nambara on 2020/11/02.
+//  Copyright © 2020 Kento Nambara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
